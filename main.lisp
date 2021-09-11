076b70c520c55c7722921b010d1286b7562fd4f7
wcourtney31415 <wcourtney31415@gmail.com>
2021-09-11 16:20:07 -0400

    Initial Commit.

---
+++ main.lisp
@@ -0,0 +1,54 @@
+( defun ret (x) (reverse (reverse x)) )
+
+( print "What base?" )
+( setq base 
+    ( read ) )
+( print "What number?" )
+( setq myNumber 
+    ( read ) )
+
+( defun generateListRecursive 
+    ( base exponent amyList myNumber ) 
+    ( if 
+        ( = myNumber 0 ) 
+        ( ret amyList ) 
+        ( let 
+            ( 
+                ( j 
+                    ( expt base exponent ) ) ) 
+            ( if 
+                ( > j myNumber ) 
+                ( ret amyList ) 
+                ( generateListRecursive base 
+                    ( + exponent 1 ) 
+                    ( cons j amyList ) myNumber ) ) ) ) )
+
+
+
+
+
+( defun generateList 
+    ( base number ) 
+    ( generateListRecursive base 0 
+Nil number))
+
+( defun convertNumberRecursive 
+    ( base myNumber generatedList accList ) 
+    ( if 
+        ( = myNumber 0 ) 
+        ( ret  accList ) 
+        ( let* 
+            ( 
+                ( answer 
+                    ( floor 
+                        ( / myNumber 
+                            ( first  generatedList ) ) ) ) 
+                ( nextNum 
+                    ( - myNumber 
+                        ( * answer 
+                            ( first  generatedList ) ) ) ) ) 
+            ( convertNumberRecursive base nextnum (cdr generatedList) 
+                ( cons answer  accList ) ) ) ) )
+
+
+( print ( convertNumberRecursive base myNumber (generateList base myNumber) () ) )
